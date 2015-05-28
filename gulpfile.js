24c62a4beb34e116e5493f569dedcc7a4a90206f
Vison <kk801030@gmail.com>
2015-05-28 14:05:06 +0800

    deploy gulp, nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,26 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
 
-gulp.task('default', function() {
+gulp.task('default', ['browser-sync'], function() {
   // default task
 });
+
+gulp.task('browser-sync', ['nodemon'], function() {
+  browserSync.init(null, {
+    proxy: "http://localhost:3000",
+    files: ["public/**/*.*", "views/*.*", "views/**/*.*"],
+    browser: "google chrome",
+    port: 7000
+  });
+
+});
+
+gulp.task('nodemon', function() {
+  return nodemon({
+    script: './bin/www'
+  }).on('start', function(){
+
+  })
+});
+
