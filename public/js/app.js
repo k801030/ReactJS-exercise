c17a06113748a27e70105c885c32b0ad140bc26d
Vison <kk801030@gmail.com>
2015-05-28 16:39:32 +0800

    added commentBox

--- public/js/app.js
+++ public/js/app.js
@@ -3,15 +3,58 @@ React.render(
   document.getElementById('welcome')
 );
 
+
+// comment box
 var commentBox = React.createClass({displayName: 'CommentBox',
   render: function() {
     return(
       React.createElement('div', {className: "commentBox"}, 
-        "This is a comment box."
-        )
+        React.createElement(commentList),
+        React.createElement(commentForm)
+      )
     );
   }
 });
+
+// commentList
+var commentList = React.createClass({displayName: 'CommentList',
+  render: function() {
+    return(
+      React.createElement('div', {className: "commentList"},
+        React.createElement(comment, {author: "Vison"},
+          "Hello, I am Vison."
+        ),
+        React.createElement(comment, {author: "Jessy"},
+          "Nice to meet you."
+        )
+      )
+    );
+  }  
+})
+
+// commentForm
+var commentForm = React.createClass({displayName: 'CommentForm',
+  render: function() {
+    return(
+      React.createElement('div', {className: "commentForm"})
+    );
+  }  
+})
+
+
+// comments
+var comment = React.createClass({displayName: 'Comment',
+  render: function() {
+    return(
+      React.createElement('div', {className: 'comment'},
+        React.createElement('h2', {className: 'commentAuthor'},
+          this.props.author
+        ),
+        this.props.children
+      )
+    );
+  }
+})
 
 React.render(
   React.createElement(commentBox, null),
